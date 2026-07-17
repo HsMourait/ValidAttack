607db309a93ac48c3b60674216cd8cd915862050
HsMourait <Hasil1303@outlook.com>
2026-07-18 04:39:45 +0800

    修复无配置界面且变更版本号

--- src/main/java/com/hsmourait/valid_attack/ValidAttack.java
+++ src/main/java/com/hsmourait/valid_attack/ValidAttack.java
@@ -9,6 +9,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 
 @Mod(value = ValidAttack.MODID, dist = Dist.CLIENT)
@@ -19,5 +21,7 @@ public class ValidAttack {
     public ValidAttack(IEventBus modEventBus, ModContainer modContainer) {
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
+        // Register the config screen so the in-game Mods menu button works
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 }
--- src/main/java/com/hsmourait/valid_attack/ValidAttackClient.java
+++ src/main/java/com/hsmourait/valid_attack/ValidAttackClient.java
@@ -3,18 +3,11 @@ package com.hsmourait.valid_attack;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @EventBusSubscriber(modid = ValidAttack.MODID, value = Dist.CLIENT)
 public class ValidAttackClient {
-    public ValidAttackClient(ModContainer container) {
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
-
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         ValidAttack.LOGGER.info("HELLO FROM CLIENT SETUP");
